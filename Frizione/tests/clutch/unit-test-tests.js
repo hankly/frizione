c09205eaea65ee5091c628f5cb714d0fd85d7191
jhl.verona <jhl.verona@gmail.com>
2008-06-17 07:46:23 +0000

    Tag for Release 0.1.1

--- Frizione/tests/clutch/unit-test-tests.js
+++ Frizione/tests/clutch/unit-test-tests.js
@@ -1,46 +1,10 @@
 /*
-Copyright (c) 2008 John Leach
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
 */
 
 /*
-Copyright (c) 2008 John Leach
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+ */
 
 /*jslint evil: true */
 /*global clutch, JSON */
@@ -48,87 +12,6 @@ THE SOFTWARE.
 if (!this.clutch) {
     clutch = {};
 }
-
-clutch.date = {
-    toStandardJSON: function () {
-        function tens(n) {
-            // Format integers to have at least two digits.
-            return n < 10 ? '0' + n : n;
-        }
-
-        function hundreds(n) {
-            // Format integers to have at least three digits.
-            return n < 100 ? '0' + tens(n) : n;
-        }
-
-        Date.prototype.toJSON = function () {
-
-            return this.getUTCFullYear()  + '-' +
-                 tens(this.getUTCMonth() + 1) + '-' +
-                 tens(this.getUTCDate())  + 'T' +
-                 tens(this.getUTCHours()) + ':' +
-                 tens(this.getUTCMinutes()) + ':' +
-                 tens(this.getUTCSeconds()) + '.' +
-                 hundreds(this.getUTCMilliseconds()) + 'Z';
-        };
-    },
-
-    toMicrosoftJSON: function () {
-        Date.prototype.toJSON = function () {
-            return "\\/Date(" + this.getTime() + ")\\/";
-        };
-    },
-
-    toClutchJSON: function () {
-        function tens(n) {
-            // Format integers to have at least two digits.
-            return n < 10 ? '0' + n : n;
-        }
-
-        function hundreds(n) {
-            // Format integers to have at least three digits.
-            return n < 100 ? '0' + tens(n) : n;
-        }
-
-        Date.prototype.toJSON = function () {
-            return "\\/Date(" +
-                 this.getUTCFullYear()  + '-' +
-                 tens(this.getUTCMonth() + 1) + '-' +
-                 tens(this.getUTCDate())  + 'T' +
-                 tens(this.getUTCHours()) + ':' +
-                 tens(this.getUTCMinutes()) + ':' +
-                 tens(this.getUTCSeconds()) + '.' +
-                 hundreds(this.getUTCMilliseconds()) + 'Z' +
-                 ")\\/";
-        };
-    },
-
-    evalJSON: function () {
-        // Prototype
-        if (String.prototype.evalJSON && typeof String.prototype.evalJSON === 'function') {
-            var microsoftDate = new RegExp("^\\\\\\/Date\\((\\d+)\\)\\\\\\/$", "gm");
-            var clutchDate = new RegExp("^\\\\\\/Date\\((\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?Z\\)\\\\\\/$", "gm");
-
-            String.prototype.evalJSON = function(sanitize) {
-              var json = this.unfilterJSON();
-              try {
-                if (!sanitize || json.isJSON()) {
-                    json = json.replace(microsoftDate, function (str, p1, offset, s) {
-                        return "new Date(" + p1 + ")";
-                    });
-                    json = json.replace(clutchDate, function (str, p1, p2, p3, p4, p5, p6, p7, offset, s) {
-                        var millis = p7 || ".0";
-                        millis = millis.slice(1);
-                        return "new Date(Date.UTC(" + (+p1) + ", " + (+p2 - 1) + ", " + (+p3) + ", " + (+p4) + ", " + (+p5) + ", " + (+p6) + ", " + (+millis) + "))";
-                    });
-                    return new Function('return (' + json + ');')();
-                }
-              } catch (e) { }
-              throw new SyntaxError('Badly formed JSON string: ' + this.inspect());
-            };
-        }
-    }
-};
 
 clutch.string = {
     trim: function (string) {
@@ -145,7 +28,6 @@ clutch.string = {
     },
 
     toJSON: function (object) {
-        // Prototype
         if (Object.toJSON && typeof Object.toJSON === 'function') {
             return Object.toJSON(object);
         }
@@ -155,32 +37,11 @@ clutch.string = {
     },
 
     fromJSON: function (string) {
-        // Prototype
         if (String.prototype.evalJSON && typeof String.prototype.evalJSON === 'function') {
             return string.evalJSON(true);
         }
         else {
-            var microsoftDate = new RegExp("^\\\\\\/Date\\((\\d+)\\)\\\\\\/$", "gm");
-            var clutchDate = new RegExp("^\\\\\\/Date\\((\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?Z\\)\\\\\\/$", "gm");
-
-            return JSON.parse(string, function (key, value) {
-                var match, millis;
-                if (typeof value === 'string') {
-                    match = microsoftDate.exec(value);
-                    if (match) {
-                        return new Date(+match[1]);
-                    }
-                    else {
-                        match = clutchDate.exec(value);
-                        if (match) {
-                            millis = match[7] || ".0";
-                            millis = match[7].slice(1);
-                            return new Date(Date.UTC(+match[1], +match[2] - 1, +match[3], +match[4], +match[5], +match[6], +millis));
-                        }
-                    }
-                }
-                return value;
-            });
+            return JSON.parse(string);
         }
     },
 
--- Ruby/compressor_servlet.rb
+++ Ruby/compressor_servlet.rb
@@ -1,41 +1,14 @@
-# Copyright (c) 2008 John Leach
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-## User: john.leach
+# User: john.leach
 
-require 'create_dirs'
 require 'erb'
-require 'jsmin'
 require 'webrick'
 
 include WEBrick
 
 class CompressorServlet < HTTPServlet::AbstractServlet
 
-    COMPRESSOR = "java -jar ../Java/yuicompressor-2.3.5.jar"
-
-    include CreateDirs
-
     def initialize(server, *options)
         super(server, options)
-        @error = ""
         @html = ERB.new <<-xXx
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -58,7 +31,6 @@ class CompressorServlet < HTTPServlet::AbstractServlet
         <% if (error) %>
         <p>Error: <%= error %></p>
         <% else %>
-        <p><code>&#160;&#160;&#160;&#160;&#160;&#160;Used:</code> <%= compressor %></p>
         <p><code>&#160;&#160;Original:</code> <%= original %> (<%= original_size %> bytes)</p>
         <p><code>Compressed:</code> <%= output %> (<%= output_size %> bytes)</p>
         <p><code>&#160;&#160;&#160;&#160;Status:</code> <%= status %></p>
@@ -77,79 +49,32 @@ class CompressorServlet < HTTPServlet::AbstractServlet
 xXx
     end
 
-    def runCompressor(input_filepath, output_filepath, options)
-        begin
-            results = %x{#{COMPRESSOR} #{options} #{input_filepath}}
-            return $?
-        rescue
-            @error = "Couldn't run YUICompressor: #{COMPRESSOR} #{options} #{input_filepath}"
-            return -1
-        end
-    end
-
-    def runJSMin(input_filepath, output_filepath)
-        input = nil
-        output = nil
-        begin
-            create_dirs(output_filepath)
-            File.delete(output_filepath) if File.exists?(output_filepath)
-            input = File.new(input_filepath, "r")
-            output = File.new(output_filepath, "w")
-            JSMin.new(input, output)
-            return 0
-        rescue
-            @error = "Couldn't execute JSMin: #{input_filepath} #{output_filepath}"
-            return -1
-        ensure
-            input.close unless input.nil?
-            output.close unless output.nil?
-        end
-    end
-
     def do_POST(request, response)
         original = request.path_info
         original = original[1..-1] if original[0...1] == '/'
         line_break = request.query['line-break'] || 0.to_s
         charset = request.query['charset'] || 'UTF-8'
-        output = request.query['output'] || nil
-        output = output[1..-1] if output && output[0...1] == '/'
+        output = request.query['output'] || original
+        output = output[1..-1] if output[0...1] == '/'
         nomunge = request.query['nomunge'] || true;
         nomunge = false if (nomunge) == 'false'
-        jsmin = request.query['jsmin'] || true;
-        jsmin = false if (jsmin) == 'false'
-        @logger.info "run-compressor source: #{original}, #{jsmin}"
 
         original_filepath = File.expand_path(original, ClutchServer::ROOT)
-        original_size = File.size(original_filepath)
-        output_filepath = nil
-        @error = nil
         if (!File.exists?(original_filepath))
-            @error = "run-compressor: #{request.path_info} doesn't exist"
-        elsif (output.nil?)
-            @error = "run-compressor: no output parameter specified"
-        elsif (output)
+            error = "#{request.path_info} doesn't exist!"
+        else
             output_filepath = File.expand_path(output, ClutchServer::ROOT)
-            if (original_filepath == output_filepath)
-                @error = "run-compressor: #{request.path_info} cannot be written to itself"
-            end
-        end
+            original_size = File.size(original_filepath)
 
-        output_size = 0
-        compressor = "JSMin"
-        if (@error.nil?)
-            if (jsmin)
-                status = runJSMin(original_filepath, output_filepath)
-            else
-                compressor = "YUICompressor"
-                options = '--line-break ' + line_break + ' --charset ' + charset + ' -o ' + output_filepath
-                options += ' --nomunge' if (nomunge)
-                # See http://developer.yahoo.com/yui/compressor/
-                status = runCompressor(original_filepath, output_filepath, options)
-            end
-            output_size = File.size(output_filepath) if File.exists?(output_filepath)
-        end
+            options = '--line-break ' + line_break + ' --charset ' + charset + ' -o ' + output_filepath
+            options += ' --nomunge' if (nomunge)
+            @logger.info "run-compressor source: #{original}, #{options}"
 
-        error = @error
+            # See http://developer.yahoo.com/yui/compressor/
+            results = %x{java -jar ../Java/yuicompressor-2.3.5.jar #{options} #{original_filepath} }
+            status = $?
+            output_size = File.size(output_filepath)
+        end
         response.content_type = 'text/html'
         response.body = @html.result(binding)
     end
